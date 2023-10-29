356d36a0bd2eee57bbc6bb978a405fb13b4c28fb
ulises Uliii <ulisesuliiuwu@gmail.com>
2023-10-29 10:06:09 -0600

    archivos de configuracion del carrito , permisos y debugar

---
+++ app/Models/Producto.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+      'nombre_producto',
+      'descripcion',
+      'precio',
+      'stock',
+      'estado',
+      'fecha_activo',
+      'fecha_limite',
+      'imagen',
+  ];
+}
